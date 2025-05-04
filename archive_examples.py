5d0780fd560077272582ffd24c7a16ed8b4b6ea2
lobotomyx <lbatvt@gmail.com>
2025-05-04 14:13:04 -0400

    Update and rename archive_examples.py to tools/archive_examples.py

--- archive_examples.py
+++
@@ -1,5 +0,0 @@
-import shutil
-from os.path import join, getcwd
-from sys import argv
-shutil.make_archive(f'Examples32.zip', 'zip', join(getcwd(), "bin", "Win32", "Release Examples"))
-shutil.make_archive(f'Examples64.zip', 'zip', join(getcwd(), "bin", "x64", "Release Examples"))
